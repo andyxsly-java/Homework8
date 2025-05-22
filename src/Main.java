ab0f3530095238632154470ffc75898706318e36
andyxsly <andyxsly@bk.ru>
2025-05-22 21:01:48 +0300

    Complete homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Задание 1, пример 1
+        int[] value = new int[3];
+        value[0] = 1;
+        value[1] = 2;
+        value[2] = 3;
+        System.out.println(value[0]);
+        System.out.println(value[1]);
+        System.out.println(value[2]);
+        //Пример 2
+        double[] meaning = {1, 2, 3};
+        meaning[0] = 1.57;
+        System.out.println(meaning[0]);
+        meaning[1] = 7.654;
+        System.out.println(meaning[1]);
+        meaning[2] = 9.986;
+        System.out.println(meaning[2]);
+        //Пример 3
+        int[] sense = new int[5];
+        sense[0] = 33;
+        int apple = 0;
+        System.out.println(sense[apple]);
+        //Задание 2
+        int[] numbers = new int[]{1, 2, 3};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        //Задание 3
+        int[] symbols = new int[]{1, 2, 3};
+        for (int i = symbols.length - 1; i >= 0; i--) {
+            System.out.print(symbols[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        //Задание 4
+        int[] edition = new int[]{1, 2, 3};
+        for (int i = 0; i < edition.length; i++) {
+            if (edition[i] % 2 != 0) {
+                edition[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(edition));
     }
 }
